69e4ff1213041e12341cd460f030f2a5e166f8ec
nmakarin <n-makarin@mail.ru>
2019-09-15 19:10:52 +0300

    Remove scaffolds files, create pages/index.vue Remove logo.png, HelloWorld.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <index />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Index from './components/pages/index'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Index
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html, body {
+  margin: 0;
+  padding: 0;
 }
 </style>
