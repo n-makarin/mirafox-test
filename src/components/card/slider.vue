37a1ccd642a60e1e15e6967f9d5c7fe0e42b7f61
nmakarin <n-makarin@mail.ru>
2019-09-15 22:40:38 +0300

    Add card-list.vue to card/slider.vue

--- src/components/card/slider.vue
+++ src/components/card/slider.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="card-slider">
-    card-slider
+    <card-list class="card-slider__card-list card-list_slider-view" :data="slideData" />
   </div>
 </template>
 
 <script>
+import CardList from '@/components/card/list'
 
 export default {
   components: {
+    CardList
   },
   props: {
     data: {
       type: Array,
       default: () => []
+    }
+  },
+  computed: {
+    slideData() {
+      return this.data
     }
   }
 }
