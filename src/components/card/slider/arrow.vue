1e3a71e48aad0f28dfd952df7288af8c88120084
nmakarin <n-makarin@mail.ru>
2019-09-15 23:04:07 +0300

    Add btn hover and change height in arrow.vue src\components\card\slider\arrow.vue

--- src/components/card/slider/arrow.vue
+++ src/components/card/slider/arrow.vue
@@ -23,15 +23,19 @@ export default {
 
 <style lang="scss" scoped>
 .card-slider__arrow {
+  z-index: 2;
   button {
     width: 40px;
-    height: 100px;
+    height: 80px;
     border-radius: 3px;
     background: #a0a0a0;
     color: #e4e4e4;
     border: none;
     font-size: 27px;
     cursor: pointer;
+    &:hover {
+      background: #949494;
+    }
     div {
       display: block;
       transform: scale(1, 2.5)
