c7215dbda7df7b4cb952c31419f50617ab73bf8e
nmakarin <n-makarin@mail.ru>
2019-09-15 20:42:58 +0300

    Add card-list loop to src\components\card\list.vue

--- src/components/card/list.vue
+++ src/components/card/list.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="card-list">
-    card-list
+    <item 
+      v-for="(item, index) in data" 
+      :key="index"
+      :data="item"
+    />
   </div>
 </template>
 
 <script>
+import Item from '@/components/card/item'
 
 export default {
   components: {
+    Item
   },
   props: {
     data: {
