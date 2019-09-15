3597808fd8b532aa51bbeb9f552d9f1d6ac73b41
nmakarin <n-makarin@mail.ru>
2019-09-15 19:14:44 +0300

    Add scss config to babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,22 @@
 module.exports = {
   presets: [
     '@vue/app'
-  ]
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue'
+      }, 
+      {
+          test: /\.s[a|c]ss$/,
+          loader: 'style!css!sass'
+      }
+    ]
+  },
+  vue: {
+    loaders: {
+      scss: 'style!css!sass'
+    }
+  }
 }
