b8a241670be3b3abb02efcb126444f8c43685b52
nmakarin <n-makarin@mail.ru>
2019-09-15 23:50:20 +0300

    Add ability to turn slides infinitly src\components\card\slider.vue

--- src/components/card/slider.vue
+++ src/components/card/slider.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="card-slider">
-    <arrow direction="left" />
+    <arrow @left="turnLeft" direction="left" />
     <card-list class="card-slider__card-list card-list_slider-view" :data="slideData" />
-    <arrow direction="right" />
+    <arrow @right="turnRight" direction="right" />
   </div>
 </template>
 
@@ -21,13 +21,61 @@ export default {
       default: () => []
     }
   },
+  data: () => ({
+    itemsToShow: 5,
+    itemsToLoad: 5,
+    slideNumber: 1
+  }),
   computed: {
     slideData() {
-      return this.data
+      if (this.slideNumber >= 0) {
+        return this.data.slice(this.startItemNumber, this.endItemNumber)
+      } else {
+        return this.data.slice(this.endItemNumber, this.startItemNumber)
+      }
+    },
+    /**
+     * All slides quantity
+     */
+    allSlidesNumber() {
+      return this.data.length / this.itemsToLoad
+    },
+    /**
+     * Start number to slicing from data array
+     */
+    startItemNumber() {
+      return this.slideNumber * this.itemsToLoad - this.itemsToLoad
+    },
+    /**
+     * End number to slicing from data array
+     */
+    endItemNumber() {
+      return this.startItemNumber + this.itemsToShow
+    }
+  },
+  methods: {
+    turnLeft() {
+      if (this.slideNumber - 1 >= 1) {
+        this.slideNumber--
+      } else {
+        this.slideNumber = this.allSlidesNumber
+      }
+    },
+    turnRight() {
+      if (this.slideNumber + 1 <= this.allSlidesNumber) {
+        this.slideNumber++
+      } else {
+        this.slideNumber = 1
+      }
     }
   }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.card-slider {
+  display: flex;
+  align-items: center;
+  overflow: hidden;
+}
 </style>
